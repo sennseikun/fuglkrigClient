678a02c2eca5eafa1d7453ad20370b70a02e290b
fomox <magnugut@stud.ntnu.no>
2017-03-31 10:11:00 +0200

    fixed unnessesary sending of data

--- app/src/main/java/com/example/simon/client/UpdateServer.java
+++ app/src/main/java/com/example/simon/client/UpdateServer.java
@@ -31,6 +31,32 @@ public class UpdateServer extends Thread {
         running = false;
     }
 
+    public boolean compareJson(JSONObject obj1, JSONObject obj2) {
+        boolean different = false;
+
+        //makes sure the objects are not null (which they are in the start of the game
+        if (obj1 == null || obj2 == null) {
+            System.out.println("Didnt send data because null");
+            return true;
+        }
+
+        try {
+            //checks if X is different
+            if (obj1.get("TargetX") != obj2.get("TargetX")) {
+                different = true;
+            }
+            //checks if Y is different
+            else if (obj1.get("TargetY") != obj2.get("TargetY")) {
+                different = true;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return different;
+    }
+
     @Override
     public void run(){
 
@@ -45,11 +71,13 @@ public class UpdateServer extends Thread {
                 sendData.put("TargetX",DataModel.getTargetX());
                 sendData.put("TargetY",DataModel.getTargetY());
 
-                if(lastSent != sendData){
+                if(compareJson(lastSent, sendData)){
+                    System.out.println("last: " + lastSent);
+                    System.out.println("New: " + sendData);
                     handler.sendData(sendData);
                     lastSent = sendData;
-                    System.out.println("Sent X: "+DataModel.getTargetX());
-                    System.out.println("Sent Y: "+DataModel.getTargetY());
+                    //System.out.println("Sent X: "+DataModel.getTargetX());
+                    //System.out.println("Sent Y: "+DataModel.getTargetY());
                 }
 
             } catch (JSONException e) {
